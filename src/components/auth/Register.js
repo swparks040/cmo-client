b79be1a0282870dd2b9f8eb21afe66d1ad226f6a
Scott Parks <swparks040@gmail.com>
2022-12-12 10:12:28 -0600

    created manager files in managers component to host code for different sections of my dashboard

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,22 +1,28 @@
-import { useRef } from "react"
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import { useRef } from "react";
 //import { Link } from "react-router-dom"
-import { useNavigate } from "react-router-dom"
-import { registerUser } from "../managers/AuthManager"
+import { Link, useNavigate } from "react-router-dom";
+import { registerUser } from "../managers/AuthManager";
 
-export const Register = ({setToken}) => {
-  const firstName = useRef()
-  const lastName = useRef()
-  const email = useRef()
-  const username = useRef()
-  const bio = useRef()
-  const password = useRef()
-  const verifyPassword = useRef()
-  const passwordDialog = useRef()
-  const navigate = useNavigate()
+export const Register = ({ setToken }) => {
+  const firstName = useRef();
+  const lastName = useRef();
+  const email = useRef();
+  const username = useRef();
+  const is_staff = useRef();
+  const password = useRef();
+  const verifyPassword = useRef();
+  const passwordDialog = useRef();
+  const navigate = useNavigate();
 
   const handleRegister = (e) => {
-    e.preventDefault()
-    
+    e.preventDefault();
+
     if (password.current.value === verifyPassword.current.value) {
       const newUser = {
         username: username.current.value,
@@ -24,22 +30,98 @@ export const Register = ({setToken}) => {
         last_name: lastName.current.value,
         email: email.current.value,
         password: password.current.value,
-        bio: bio.current.value
-      }
+        is_staff: is_staff.current.value,
+      };
 
-      registerUser(newUser)
-        .then(res => {
-          if ("valid" in res && res.valid) {
-            setToken(res.token)
-            navigate("/")
-          }
-        })
+      registerUser(newUser).then((res) => {
+        if ("valid" in res && res.valid) {
+          setToken(res.token);
+          navigate("/");
+        }
+      });
     } else {
-      passwordDialog.current.showModal()
+      passwordDialog.current.showModal();
     }
-  }
+  };
 
-  return <>Bootstrap Registration Screen 
-  <div>onSubmit={handleRegister}</div>
-  </>;
+  return (
+    <div>
+      <Container>
+        <Row className="vh-100 d-flex justify-content-center align-items-center">
+          <Col md={8} lg={6} xs={12}>
+            <Card className="px-4">
+              <Card.Body>
+                <div className="mb-3 mt-md-4">
+                  <h2 className="fw-bold mb-2 text-center text-uppercase ">
+                    Logo
+                  </h2>
+                  <div className="mb-3">
+                    <Form onSubmit={handleRegister}>
+                      <Form.Group className="mb-3" controlId="userName">
+                        <Form.Label className="text-center">
+                          Username
+                        </Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="Enter Username"
+                        />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="firstName">
+                        <Form.Label className="text-center">
+                          First Name
+                        </Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="Enter First Name"
+                        />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="lastName">
+                        <Form.Label className="text-center">
+                          Last Name
+                        </Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="Enter Last Name"
+                        />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="text-center">
+                          Email address
+                        </Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </Form.Group>
+
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicPassword"
+                      >
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                      </Form.Group>
+                      <div className="d-grid">
+                        <Button variant="primary" type="submit">
+                          Create Account
+                        </Button>
+                      </div>
+                    </Form>
+                    <div className="mt-3">
+                      <p className="mb-0  text-center">
+                        Already Registered?{" "}
+                        <Link to="/login" className="text-primary fw-bold">
+                          Sign In
+                        </Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
